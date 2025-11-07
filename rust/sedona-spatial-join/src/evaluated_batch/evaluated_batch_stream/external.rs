9375174728d94c9b98927bb88ab001f3c69a68bf
Kristin Cowalcijk <bo@wherobots.com>
2025-11-08 02:18:32 +0800

    Clean up the code for reading spilled evaluated batches

--- rust/sedona-spatial-join/src/evaluated_batch/evaluated_batch_stream/external.rs
+++ rust/sedona-spatial-join/src/evaluated_batch/evaluated_batch_stream/external.rs
@@ -34,7 +34,6 @@ pub(crate) struct ExternalEvaluatedBatchStream {
 }
 
 enum State {
-    Invalid,
     UnInitialized(RefCountedTempFile),
     Opening(SpawnedTask<Result<SpillReader>>),
     Reading(SpawnedTask<(SpillReader, Option<Result<EvaluatedBatch>>)>),
@@ -62,67 +61,49 @@ impl futures::Stream for ExternalEvaluatedBatchStream {
         let self_mut = self.get_mut();
 
         loop {
-            match &mut self_mut.state {
-                State::UnInitialized(_) => {
-                    let State::UnInitialized(spill_file) =
-                        std::mem::replace(&mut self_mut.state, State::Invalid)
-                    else {
-                        unreachable!()
-                    };
+            match std::mem::replace(&mut self_mut.state, State::Finished) {
+                State::UnInitialized(spill_file) => {
                     let task =
                         SpawnedTask::spawn_blocking(move || SpillReader::try_new(&spill_file));
                     self_mut.state = State::Opening(task);
                 }
-                State::Opening(task) => {
-                    let join_result = futures::ready!(task.poll_unpin(cx));
-                    match join_result {
-                        Err(e) => {
-                            self_mut.state = State::Finished;
-                            return Poll::Ready(Some(Err(DataFusionError::External(Box::new(e)))));
-                        }
-                        Ok(open_result) => match open_result {
-                            Ok(mut spill_reader) => {
-                                let task = SpawnedTask::spawn_blocking(move || {
-                                    let next_batch = spill_reader.next_batch();
-                                    (spill_reader, next_batch)
-                                });
-                                self_mut.state = State::Reading(task);
-                            }
-                            Err(e) => {
-                                self_mut.state = State::Finished;
-                                return Poll::Ready(Some(Err(e)));
-                            }
-                        },
+                State::Opening(mut task) => match futures::ready!(task.poll_unpin(cx)) {
+                    Err(e) => {
+                        return Poll::Ready(Some(Err(DataFusionError::External(Box::new(e)))));
                     }
-                }
-                State::Reading(task) => {
-                    let join_result = futures::ready!(task.poll_unpin(cx));
-                    match join_result {
-                        Err(e) => {
-                            self_mut.state = State::Finished;
-                            return Poll::Ready(Some(Err(DataFusionError::External(Box::new(e)))));
-                        }
-                        Ok((mut spill_reader, next_batch_result)) => match next_batch_result {
-                            None => {
-                                self_mut.state = State::Finished;
-                                return Poll::Ready(None);
-                            }
-                            Some(Err(e)) => {
-                                self_mut.state = State::Finished;
-                                return Poll::Ready(Some(Err(e)));
-                            }
-                            Some(Ok(batch)) => {
-                                let task = SpawnedTask::spawn_blocking(move || {
-                                    let next_batch = spill_reader.next_batch();
-                                    (spill_reader, next_batch)
-                                });
-                                self_mut.state = State::Reading(task);
-                                return Poll::Ready(Some(Ok(batch)));
-                            }
-                        },
+                    Ok(Err(e)) => {
+                        return Poll::Ready(Some(Err(e)));
                     }
+                    Ok(Ok(mut spill_reader)) => {
+                        let task = SpawnedTask::spawn_blocking(move || {
+                            let next_batch = spill_reader.next_batch();
+                            (spill_reader, next_batch)
+                        });
+                        self_mut.state = State::Reading(task);
+                    }
+                },
+                State::Reading(mut task) => match futures::ready!(task.poll_unpin(cx)) {
+                    Err(e) => {
+                        return Poll::Ready(Some(Err(DataFusionError::External(Box::new(e)))));
+                    }
+                    Ok((_, None)) => {
+                        return Poll::Ready(None);
+                    }
+                    Ok((_, Some(Err(e)))) => {
+                        return Poll::Ready(Some(Err(e)));
+                    }
+                    Ok((mut spill_reader, Some(Ok(batch)))) => {
+                        let task = SpawnedTask::spawn_blocking(move || {
+                            let next_batch = spill_reader.next_batch();
+                            (spill_reader, next_batch)
+                        });
+                        self_mut.state = State::Reading(task);
+                        return Poll::Ready(Some(Ok(batch)));
+                    }
+                },
+                State::Finished => {
+                    return Poll::Ready(None);
                 }
-                _ => todo!(),
             }
         }
     }
