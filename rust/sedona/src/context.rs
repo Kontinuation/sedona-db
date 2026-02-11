96989a4f68119c678dd206435df5ea2083b963df
Kristin Cowalcijk <bo@wherobots.com>
2026-02-12 00:17:02 +0800

    Fix rebase automerge

--- rust/sedona/src/context.rs
+++ rust/sedona/src/context.rs
@@ -108,9 +108,6 @@ impl SedonaContext {
                 .with_las_extra_bytes(LasExtraBytes::Typed),
         );
 
-        let rt_builder = RuntimeEnvBuilder::new();
-        let runtime_env = rt_builder.build_arc()?;
-
         #[allow(unused_mut)]
         let mut state_builder = SessionStateBuilder::new()
             .with_default_features()
